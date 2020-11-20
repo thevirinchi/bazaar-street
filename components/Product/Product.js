6f7667e3e12d883bc48950c0a11623e888f12818
z3t4z00k <z3t4z00k@gmail.com>
2020-11-20 11:06:34 -0500

    Improved styling

--- components/Product/Product.js
+++ components/Product/Product.js
@@ -5,30 +5,34 @@ import Body from '../Typo/Body'
 import Button from '../Button/Button'
 
 import { Colors } from '../../constants/colors'
-import { Margin } from '../../constants/utility'
+import { Margin, Padding } from '../../constants/utility'
 
 const Product = props => {
 	return (
 		<View style={{ ...styles.container }}>
 			<View style={{ ...styles.imageContainer }} onPress={() => { props.onOpenHandler(props.id) }}><Image source={{ uri: props.imageUrl }} style={{ ...styles.image }} /></View>
-			<TouchableOpacity onPress={() => { props.onAddHandler(props.id) }}>
-				<View style={{ ...styles.button }}>
-					<Body lvl={3} text="Add to cart" />
-				</View>
-			</TouchableOpacity>
-			<View style={{ ...styles.nameContainer }}><Body lvl={1} text={props.name} style={{ marginVertical: 0 }} /></View>
-			<View style={{ ...styles.priceContainer }}><Body lvl={4} text={"₹" + props.price} /></View>
+			<View style={{...styles.detailsContainer}}>
+				<Body lvl={1} text={props.name} style={{ marginVertical: 0 }} />
+				<Body lvl={4} text={"₹" + props.price} style={{backgroundColor: Colors.secondary, paddingHorizontal: Padding.s}} />
+			</View>
+			<View style={{...styles.buttonContainer}}>
+				<TouchableOpacity onPress={() => { props.onAddHandler(props.id) }}>
+					<Button lvl={1} text="Add to cart"/>
+				</TouchableOpacity>
+				<TouchableOpacity onPress={() => { props.onOpenHandler(props.id) }}>
+					<Button lvl={2} text="View Details"/>
+				</TouchableOpacity>
+			</View>
 		</View>
 	)
 }
 
 const styles = StyleSheet.create({
-	container: { width: "100%", flex: 1, justifyContent: "center", marginHorizontal: Margin.s, marginBottom: Margin.l },
+	container: { flex: 1, justifyContent: "center", elevation: 4, margin: Margin.l, backgroundColor: Colors.whiteLight, borderRadius: 8, overflow: "hidden"},
 	imageContainer: { width: "100%", alignItems: "center" },
 	image: { width: "100%", height: 200 },
-	nameContainer: { width: "100%", alignItems: "center" },
-	priceContainer: { width: "100%", alignItems: "center" },
-	button: { borderColor: Colors.black, borderWidth: 1.5, width: "100%", justifyContent: "center", alignItems: "center" }
+	detailsContainer: {flex: 1, width: "100%", flexDirection: "row", justifyContent: "space-between", alignItems: "center", paddingHorizontal: Padding.xl, marginVertical: Margin.m},
+	buttonContainer: { flex: 1, width: "100%", flexDirection: "row", justifyContent: "space-between", alignItems: "center", paddingHorizontal: Padding.xl, marginBottom: Margin.l },
 })
 
 export default Product
