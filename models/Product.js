8aa8efc6bdd211212dfb33ba6104835d5d0f1715
z3t4z00k <z3t4z00k@gmail.com>
2020-11-20 07:12:13 -0500

    Created Product Model

---
+++ models/Product.js
@@ -0,0 +1,12 @@
+class ProductModel {
+	constructor(id, uid, name, imageURL, desc, price){
+		this.id = id
+		this.uid = uid
+		this.imageURL = imageURL
+		this.name = name
+		this.desc = desc
+		this.price = price
+	}
+}
+
+export default ProductModel
