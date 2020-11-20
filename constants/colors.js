df02d6923505b396ab2d607d2c17785815b5cd29
z3t4z00k <z3t4z00k@gmail.com>
2020-11-20 02:26:17 -0500

    Created color scheme

---
+++ constants/colors.js
@@ -0,0 +1,15 @@
+export const Colors = {
+	primary: '#2196f3',
+	primaryLight: '#6ec6ff',
+	primaryDark: '#0069c0',
+	secondary: '#fdd835',
+	secondaryLight: '#ffff6b',
+	secondaryDark: '#c6a700',
+	accent: '#69f0ae',
+	white: '#fafafa',
+	whiteLight: '#ffffff',
+	whiteDark: '#c7c7c7',
+	black: '#212121',
+	blackLight: '#484848',
+	blackDark: '#000000'
+}
