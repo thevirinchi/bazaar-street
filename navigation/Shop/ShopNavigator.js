62d2e8b4450ae76a8849f4c8b19b96a96617d0be
z3t4z00k <z3t4z00k@gmail.com>
2020-11-20 09:06:42 -0500

    Renamed navigator

--- navigation/Shop/ShopNavigator.js
+++ navigation/Shop/ShopNavigator.js
@@ -7,9 +7,18 @@ import { createDrawerNavigator } from 'react-navigation-drawer'
 import { createAppContainer } from 'react-navigation'
 
 import Products from '../../screens/Shop/Products'
+import ProductDetails from '../../screens/Shop/Product'
+import { Colors } from '../../constants/colors';
 
-const ProductsNavigator = createStackNavigator({
+const ShopNavigator = createStackNavigator({
 	Products: Products
+}, {
+	defaultNavigationOptions: {
+		headerStyle: {
+			backgroundColor: Colors.primary
+		},
+		headerTintColor: Colors.blackDark
+	}
 })
 
-export default createAppContainer(ProductsNavigator)
+export default createAppContainer(ShopNavigator)
