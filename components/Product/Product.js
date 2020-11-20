68b9af84bf51943732a1cb1970f2547e8dffcc23
z3t4z00k <z3t4z00k@gmail.com>
2020-11-20 07:40:22 -0500

    Improved Product component

--- components/Product/Product.js
+++ components/Product/Product.js
@@ -2,19 +2,26 @@ import React from 'react'
 import {View, Text, StyleSheet, Image} from 'react-native'
 
 import Body from '../Typo/Body'
+import Button from '../Button/Button'
+import { Margin } from '../../constants/utility'
 
 const Product = props => {
 	return (
-		<View style={{width: "100%", flex: 1, alignItems:"center", justifyContent:"center", marginVertical: 8}}>
-			<Image source={{uri: props.imageUrl}} style={{width: "80%", height: 300}}/>
-			<Body lvl={1} text={props.name}/>
-			<Body lvl={4} text={props.price}/>
+		<View style={{...styles.container}}>
+			<View style={{...styles.imageContainer}}><Image source={{uri: props.imageUrl}} style={{...styles.image}}/></View>
+			<Button text="Add to cart"/>
+			<View style={{...styles.nameContainer}}><Body lvl={1} text={props.name} style={{marginVertical: 0}}/></View>
+			<View style={{...styles.priceContainer}}><Body lvl={4} text={"₹"+props.price}/></View>
 		</View>
 	)
 }
 
 const styles = StyleSheet.create({
-
+	container: {width: "100%", flex: 1, justifyContent:"center", marginHorizontal: Margin.s, marginBottom: Margin.l},
+	imageContainer: {width: "100%", alignItems: "center"},
+	image: {width: "100%", height: 200},
+	nameContainer: {width: "100%", alignItems: "center"},
+	priceContainer: {width: "100%", alignItems: "center"}
 })
 
 export default Product
