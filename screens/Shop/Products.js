cd4298289833bbc459c371885180a85f1959ce5b
z3t4z00k <z3t4z00k@gmail.com>
2020-11-21 03:59:57 -0500

    Added add to cart functionality

--- screens/Shop/Products.js
+++ screens/Shop/Products.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { FlatList } from 'react-native-gesture-handler'
 import { HeaderButtons, Item } from 'react-navigation-header-buttons'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 
 import HeaderNavButton from '../../components/Header/Button'
 
@@ -9,19 +9,19 @@ import Product from '../../components/Product/Product'
 
 import { Colors } from '../../constants/colors'
 
+import * as cartActions from '../../store/actions/cart'
+
 const Products = props => {
 
-	const products = useSelector(state => state.products.availableProds)
+	const dispatch = useDispatch()
 
-	const onAddHandler = id => {
-		console.log(id)
-	}
+	const products = useSelector(state => state.products.availableProds)
 
 	const renderProduct = itemData => {
 		return (
 			<Product
 				onOpenHandler={() => { props.navigation.navigate('ProductDetails', { productId: itemData.item.id, title: itemData.item.name }) }}
-				onAddHandler={onAddHandler}
+				onAddHandler={() => { console.log("Clicked"); dispatch(cartActions.addToCart(itemData.item)) }}
 				id={itemData.item.id}
 				uid={itemData.item.uid}
 				name={itemData.item.name}
@@ -39,7 +39,7 @@ const Products = props => {
 Products.navigationOptions = navData => {
 	return {
 		headerLeft: () => <HeaderButtons HeaderButtonComponent={HeaderNavButton}><Item title="Menu" iconName='ios-menu' onPress={() => { console.log("drawer") }} /></HeaderButtons>,
-		headerRight: () => <HeaderButtons HeaderButtonComponent={HeaderNavButton}><Item title="Menu" iconName='ios-cart' onPress={() => { console.log("cart") }} /></HeaderButtons>
+		headerRight: () => <HeaderButtons HeaderButtonComponent={HeaderNavButton}><Item title="Menu" iconName='ios-cart' onPress={() => { navData.navigation.navigate('Cart') }} /></HeaderButtons>
 	}
 }
 
