031728837001bebeda6ecd9cea06a6cd49fa623b
z3t4z00k <z3t4z00k@gmail.com>
2020-11-21 07:52:13 -0500

    Added an action and a reducer to handle deleting an item from cart

--- store/reducers/cart.js
+++ store/reducers/cart.js
@@ -1,4 +1,4 @@
-import { ADD_TO_CART } from '../actions/cart'
+import { ADD_TO_CART, REMOVE_FROM_CART } from '../actions/cart'
 import CartItem from '../../models/CartItem'
 import { add } from 'react-native-reanimated'
 
@@ -13,25 +13,49 @@ const cartReducer = (state = initState, actions) => {
 			const addProduct = actions.product
 			const prodPrice = addProduct.price
 			const prodName = addProduct.name
-			if(state.items[addProduct.id]){
+			if (state.items[addProduct.id]) {
 				const updateCartItem = new CartItem(
-					state.items[addProduct.id].quantity+1,
+					state.items[addProduct.id].quantity + 1,
 					prodPrice,
 					prodName,
 					state.items[addProduct.id].sum + prodPrice
 				)
 				return {
 					...state,
-					items: {...state.items, [addProduct.id]: updateCartItem},
+					items: { ...state.items, [addProduct.id]: updateCartItem },
 					totalAmount: state.totalAmount + prodPrice
 				}
 			}
-			else{
+			else {
 				const newCartItem = new CartItem(1, prodPrice, prodName, prodPrice)
 				return {
 					...state,
-					items: {...state.items, [addProduct.id]: newCartItem},
+					items: { ...state.items, [addProduct.id]: newCartItem },
 					totalAmount: state.totalAmount + prodPrice
+				}
+			}
+		case REMOVE_FROM_CART:
+			const qty = state.items[actions.pid].quantity
+			if (qty > 1) {
+				const updateCartItem = new CartItem(
+					state.items[actions.pid].quantity - 1,
+					state.items[actions.pid].price,
+					state.items[actions.pid].title,
+					state.items[actions.pid].sum - state.items[actions.pid].price
+				)
+				return {
+					...state,
+					items: { ...state.items, [actions.pid]: updateCartItem },
+					totalAmount: state.totalAmount - state.items[actions.pid].price
+				}
+			}
+			else {
+				const currentItems = { ...state.items }
+				delete currentItems[actions.pid]
+				return {
+					...state,
+					items: currentItems,
+					totalAmount: state.totalAmount - state.items[actions.pid].price
 				}
 			}
 		default:
